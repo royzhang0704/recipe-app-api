5d2bc98415da4cdbdf04414772deb5599b80c0a4
Roy <161924422+royzhang0704@users.noreply.github.com>
2024-09-27 18:53:49 +0800

    Update test.py

--- app/app/test.py
+++ app/app/test.py
@@ -3,7 +3,6 @@ from . import cal
 
 class  CalSimpleTest(SimpleTestCase):
     """Test the addition of two numbers."""
-
     def test_cal(self):
         """Test the add function."""
         res = cal.add(5, 6)
