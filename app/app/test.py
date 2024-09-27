c9b96712205cc9e4d0dc0806f32cfca18809c2cc
Roy <royzhang0704@gmail.com>
2024-09-27 18:41:04 +0800

    fix coding style

--- app/app/test.py
+++ app/app/test.py
@@ -1,9 +1,9 @@
 from django.test import SimpleTestCase
 from . import cal
 
-class CalSsmpletest(SimpleTestCase):
+class  CalSsmpletest(SimpleTestCase):
     """Test add two number"""
     def test_cal(self):
         """Test funtion"""
-        res=cal.add(5, 6)
+        res = cal.add(5 , 6)
         self.assertEqual(res, 11)
