73bcc75bbce5fb118b5db6986bebd10776fe3bb8
Roy <161924422+royzhang0704@users.noreply.github.com>
2024-09-27 18:58:33 +0800

    Update test.py

--- app/app/test.py
+++ app/app/test.py
@@ -1,9 +1,12 @@
 from django.test import SimpleTestCase
 from . import cal
 
-class  CalSimpleTest(SimpleTestCase):
+
+class CalSimpleTest(SimpleTestCase):
     """Test the addition of two numbers."""
+
     def test_cal(self):
         """Test the add function."""
         res = cal.add(5, 6)
         self.assertEqual(res, 11)
+
