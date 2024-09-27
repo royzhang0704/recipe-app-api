3b4c709c085309c87f1ed2568dea823c5dcc7ee7
Roy <royzhang0704@gmail.com>
2024-09-27 18:36:22 +0800

    Fix Lint error

--- app/app/test.py
+++ app/app/test.py
@@ -2,6 +2,8 @@ from django.test import SimpleTestCase
 from . import cal
 
 class CalSsmpletest(SimpleTestCase):
+    """Test add two number"""
     def test_cal(self):
-        res=cal.add(5,6)
-        self.assertEqual(res,11)
+        """Test funtion"""
+        res=cal.add(5, 6)
+        self.assertEqual(res, 11)
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,7 +6,6 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
-
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
@@ -16,6 +15,7 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
+
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
