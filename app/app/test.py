bcb04b1a9015c25e5d5981330e77354056b5e328
Roy <royzhang0704@gmail.com>
2024-09-27 18:49:48 +0800

    fix PEP8 #2

--- app/app/test.py
+++ app/app/test.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from . import cal
 
-class CalSimpleTest(SimpleTestCase):
+class  CalSimpleTest(SimpleTestCase):
     """Test the addition of two numbers."""
 
     def test_cal(self):
